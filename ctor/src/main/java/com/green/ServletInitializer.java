6fee6c4df041cf430c794e7272ef06996cf2c3c2
moonjikwang <shjk0315@gmail.com>
2023-03-06 09:34:27 +0900

    dir set

--- ctor/src/main/java/com/green/ServletInitializer.java
+++ ctor/src/main/java/com/green/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.ctor;
+package com.green;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
