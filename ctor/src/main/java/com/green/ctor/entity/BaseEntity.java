c50b5587b8e9678f616f47b8270fe6f2a8b3968e
22-ncore-020g <22-ncore-020g@22-ncore-020>
2023-03-06 09:36:53 +0900

    baseEntity

---
+++ ctor/src/main/java/com/green/ctor/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.green.ctor.entity;
+
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import lombok.Getter;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+abstract class BaseEntity {
+	@CreatedDate
+	@Column(name = "regDate", updatable = false)
+	private LocalDateTime regDate;
+
+	@LastModifiedDate
+	@Column(name = "modDate")
+	private LocalDateTime modDate;
+}
