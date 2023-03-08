65ffc2c2aedb6babe7f13bafcd02e58e430583e1
22-ncore-020g <22-ncore-020g@22-ncore-020>
2023-03-08 14:01:40 +0900

    create project

---
+++ ctorProject/src/main/java/com/ctor/CtorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtorProjectApplication.class, args);
+	}
+
+}
