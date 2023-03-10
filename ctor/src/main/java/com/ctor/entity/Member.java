cbe77d900729a19cd725bacfcc46e8d626e93d84
22-ncore-020g <22-ncore-020g@22-ncore-020>
2023-03-10 17:12:35 +0900

    카카오 로그인 연동

---
+++ ctor/src/main/java/com/ctor/entity/Member.java
@@ -0,0 +1,23 @@
+package com.ctor.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Member extends BaseEntity{
+
+	@Id
+	private String email;
+	private String password;
+	private String name;
+	private String profile_img;
+}
