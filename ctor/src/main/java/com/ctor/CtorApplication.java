dd14f6321785ecfa9097ec55847079cfc52f0b67
moonjikwang <shjk0315@gmail.com>
2023-03-14 13:03:14 +0900

    카카오로그인완료 익명게시판 프론트수정중

--- ctor/src/main/java/com/ctor/CtorApplication.java
+++ ctor/src/main/java/com/ctor/CtorApplication.java
@@ -2,7 +2,8 @@ package com.ctor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+@EnableJpaAuditing
 @SpringBootApplication
 public class CtorApplication {
 
--- ctor/src/main/java/com/ctor/controller/CtorController.java
+++ ctor/src/main/java/com/ctor/controller/CtorController.java
@@ -1,20 +1,37 @@
 package com.ctor.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ctor.dto.MemberDTO;
+import com.ctor.service.KakaoLoginService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class CtorController {
 
+	private final KakaoLoginService kakaoLoginService;
+	
 	@GetMapping("index")
 	public void index() {
 	}
 	@PostMapping("/register")
-	public String register(MemberDTO dto) {
+	public String register(MemberDTO dto,HttpServletRequest request) {
 		System.out.println(dto.toString());
+		String email = kakaoLoginService.register(dto);
+		if(email!= null) {
+			System.out.println(email + "회원 가입완료");
+			HttpSession session = request.getSession();
+			session.setAttribute("userInfo", dto);
+		}else {
+			System.out.println("회원가입 오류발생");
+		}
 		return "index";
 		
 	}
