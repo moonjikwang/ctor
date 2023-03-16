b690d82217eee4a736a2bd491f800c1bf93e7454
18-cocp-014g <18-cocp-014g@18-cocp-014>
2023-03-16 11:29:34 +0900

    BlindRepository 연관관계 테스트

--- ctor/src/test/java/com/ctor/blind/BlindRepositoryTests.java
+++ ctor/src/test/java/com/ctor/blind/BlindRepositoryTests.java
@@ -1,5 +1,7 @@
 package com.ctor.blind;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -8,12 +10,27 @@ import javax.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.ctor.entity.Blind;
 import com.ctor.entity.Member;
 import com.ctor.repository.BlindRepository;
 import com.ctor.repository.KakaoRepository;
-
+/**
+ * 
+ * @백승연
+ * BlindRepository 연관관계 테스트
+ * 1. 회원이 작성한 글 Insert
+ * 2. DB에 있는 blind_no의 특정 번호 조회
+ * 3. BlindRepository에 작성한 JPQL 테스트
+ * 	3_1. 특정 게시물과 해당 게시물을 작성한 회원을 조회
+ *  3_2. 특정 게시물과 해당 게시물에 속한 댓글들을 조회
+ *  3_3. 목록화면에 필요한 블라인드&멤버&댓글수를 조회
+ *  3_4. 조회화면에 필요한 블라인드&멤버&댓글수를 조회
+ */
 @SpringBootTest
 public class BlindRepositoryTests {
 
@@ -64,6 +81,62 @@ public class BlindRepositoryTests {
 		System.out.println(blind.getWriter());
 	}
 	
+	/* testReadWithWriter()
+	 * JPQL 1. 블라인드&멤버 조인한 결과를 조회하는 테스트 -> Blind의 writer변수를 이용해서 조인(연관관계O)
+	 * 특정 게시물과 해당 게시물을 작성한 회원을 조회
+	 */
+	@Test
+	public void testReadWithWriter() {
+		
+		Object result = blindRepository.getBlindWithWriter(1L);
+		
+		Object[] arr = (Object[])result;
+		
+		System.out.println("블라인드&멤버를 조인한 결과를 조회하는 테스트");
+		System.out.println(Arrays.toString(arr));
+	}
 	
+	/* testGetBlindWithComments()
+	 * JPQL 2. 블라인드&코멘츠 조인한 결과를 조회하는 테스트 -> ON키워드를 이용한 직접조인(연관관계X)
+	 * 특정 게시물과 해당 게시물에 속한 댓글들을 조회
+	 */
+	@Test
+	public void testGetBlindWithComments() {
+		
+		List<Object[]> result = blindRepository.getBlindWithComments(1L);
+		
+		for(Object[] arr : result) {
+			System.out.println(Arrays.toString(arr));
+		}
+	}
 	
+	/* testWithCommentsCount()
+	 * JPQL 3. 목록화면에 필요한 블라인드&멤버&댓글수를 조회하는 테스트
+	 * 한 페이지당 10개의 게시물, 내림차순 정렬
+	 */
+	@Test
+	public void testWithCommentsCount() {
+		
+		Pageable pageable = PageRequest.of(0, 10, Sort.by("blind_no").descending());
+		
+		Page<Object[]> result = blindRepository.getBlindWithCommentsCount(pageable);
+		
+		result.get().forEach(row -> {
+			Object[] arr = (Object[])row;
+			System.out.println(Arrays.toString(arr));
+		});
+	}
+	
+	/* testRead()
+	 * JPQL 4. 조회화면에 필요한 블라인드&멤버&댓글수를 조회하는 테스트
+	 */
+	@Test
+	public void testRead() {
+		
+		Object result = blindRepository.getBlindByBlind_no(1L);
+		
+		Object[] arr = (Object[])result;
+		
+		System.out.println(Arrays.toString(arr));
+	}
 }
