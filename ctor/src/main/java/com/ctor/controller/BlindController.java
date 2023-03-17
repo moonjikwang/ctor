981f9ff01ad77742e373cee2192bb96ea083f59b
moonjikwang <shjk0315@gmail.com>
2023-03-17 12:03:21 +0900

    더미데이터 삭제 및 db 연동출력

--- ctor/src/main/java/com/ctor/controller/BlindController.java
+++ ctor/src/main/java/com/ctor/controller/BlindController.java
@@ -1,15 +1,24 @@
 package com.ctor.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ctor.dto.BlindDTO;
+import com.ctor.dto.BlindPageRequestDTO;
+import com.ctor.service.BlindService;
 
 @Controller
 public class BlindController {
 
+	@Autowired
+	BlindService blindService;
+	
 	@GetMapping("blind")
 	public void blind() {
 	}
@@ -17,14 +26,18 @@ public class BlindController {
 	@GetMapping("blindWrite")
 	public void blindWrite() {
 	}
-	@GetMapping("blindRead")
-	public void blindRead() {
-		
+	@GetMapping({"blindRead","blindmodify"})
+	public void read(long bno, @ModelAttribute("requestDTO") BlindPageRequestDTO pageRequestDTO, Model model) {
+		/* BlindDTO dto = blindService.read(bno); */
+		/* model.addAttribute("dto",dto); */
+		model.addAttribute("requestDTO",pageRequestDTO);
 	}
 	
 	@PostMapping("blindWrite")
-	public String postImage(BlindDTO dto) {
+	public String postImage(BlindDTO dto,RedirectAttributes redirectAttributes) {
 		System.out.println("=========입력받은값 :"+dto);
-		return "redirect:blind";
+		Long bno = blindService.register(dto);
+		redirectAttributes.addAttribute("bno",bno);
+		return "redirect:blindRead";
 	}
 }
--- ctor/src/main/java/com/ctor/controller/CtorController.java
+++ ctor/src/main/java/com/ctor/controller/CtorController.java
@@ -1,14 +1,23 @@
 package com.ctor.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.ctor.dto.BoardDTO;
 import com.ctor.dto.MemberDTO;
+import com.ctor.dto.SkillDTO;
+import com.ctor.service.BoardService;
 import com.ctor.service.KakaoLoginService;
+import com.ctor.service.SkillService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,9 +26,20 @@ import lombok.RequiredArgsConstructor;
 public class CtorController {
 
 	private final KakaoLoginService kakaoLoginService;
-	
+	private final BoardService boardService;
+	private final SkillService skillService;
 	@GetMapping("index")
-	public void index() {
+	public void index(Model model) {
+		List<BoardDTO> dto = boardService.findAllBoards();
+		List<SkillDTO> skillList = skillService.getList();
+		
+		Map<String, String> skillMap = new HashMap<>();
+		for(SkillDTO skill : skillList) {
+			skillMap.put(skill.getSkill(), skill.getColor());
+		}
+		System.out.println(skillMap);
+		model.addAttribute("dto",dto);
+		model.addAttribute("skillMap",skillMap);
 	}
 	@PostMapping("/register")
 	public String register(MemberDTO dto,HttpServletRequest request) {
