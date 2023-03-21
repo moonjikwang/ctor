daccae49ca64d55ea624faec038e75c563f92cf2
18-cocp-122g <18-cocp-122g@18-cocp-122>
2023-03-21 18:21:19 +0900

    !BREAKING CHANGE: 조회쿼리 변경으로 인하여 리턴 타입 변경
    한번에 보드 엔티티와 댓글수, 멤버수를 조회하고 Object[]로 리턴시킨다.
    서비스에서 DTO에 매핑한다.

--- ctor/src/main/java/com/ctor/repository/BoardRepository.java
+++ ctor/src/main/java/com/ctor/repository/BoardRepository.java
@@ -10,24 +10,46 @@ import com.ctor.entity.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-	//모든 행 조회
-	@Query(value = "SELECT * FROM board b ORDER BY mod_date DESC", nativeQuery = true)
-	List<Board> getAll();
+	//모든 행 조회(목록)
+	@Query(value = "SELECT b "
+			+ ", COUNT(DISTINCT c) "	
+			+ ", COUNT(DISTINCT p) "
+			+ "FROM Board b "
+			+ "left join BoardComments c ON c.board = b "
+			+ "left join Participation p ON p.board = b "
+			+ "GROUP BY b  ORDER BY b.closed, b.modDate DESC")
+	List<Object[]> getAll();
 	
 	//선택한 글 조회
-		@Query(value = "SELECT b.* FROM board b WHERE b.boardno =:bno", nativeQuery = true)
-		Board getBoardByBoardno(@Param("bno")Long bno);
+		@Query(value = "SELECT b "
+				+ ", COUNT(DISTINCT c) "	
+				+ ", COUNT(DISTINCT p) "
+				+ "FROM Board b "
+				+ "left join BoardComments c ON c.board = b "
+				+ "left join Participation p ON p.board = b "
+				+ "where b.boardno =:bno ")
+				
+		Object[] getBoardByBoardno(@Param("bno")Long bno);
 	
 	//작성자Id(email)로 조회
-		@Query(value = "SELECT b.* FROM board b WHERE b.member_email =:email ORDER BY closed, mod_date DESC", nativeQuery = true)
+		@Query(value = "SELECT b.*, m.nick_name, m.profile_img, count(c.b_comment_text) "
+				+ "FROM board b, member m, board_comments c "
+				+ "where b.member_email =:email AND b.boardno = c.board_boardno AND b.member_email = m.email "
+				+ "GROUP BY b.boardno ORDER BY closed, mod_date DESC", nativeQuery = true)
 		List<Board> getBoardByEmail(@Param("email")String email);
 	
 	//기술스택으로 조회
-		@Query(value = "SELECT b.* FROM board b WHERE b.tech_stack like %:tech% ORDER BY closed, mod_date DESC", nativeQuery = true)
+		@Query(value = "SELECT b.*, m.nick_name, m.profile_img, count(c.b_comment_text) "
+				+ "FROM board b, member m, board_comments c "
+				+ "WHERE b.tech_stack like %:tech% AND b.boardno = c.board_boardno AND b.member_email = m.email "
+				+ "GROUP BY b.boardno ORDER BY closed, mod_date DESC", nativeQuery = true)
 		List<Board> getBoardByTech(@Param("tech")String tech);
 	
 	//직군으로 조회
-		@Query(value = "SELECT b.* FROM board b WHERE b.position like %:position% ORDER BY closed, mod_date DESC", nativeQuery = true)
+		@Query(value = "SELECT b.*, m.nick_name, m.profile_img, count(c.b_comment_text) "
+				+ "FROM board b, member m, board_comments c "
+				+ "WHERE b.position like %:position% AND b.boardno = c.board_boardno AND b.member_email = m.email "
+				+ "GROUP BY b.boardno ORDER BY closed, mod_date DESC", nativeQuery = true)
 		List<Board> getBoardByPosition(@Param("position")String position);
 
 }
