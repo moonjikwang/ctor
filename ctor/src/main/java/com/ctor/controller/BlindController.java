7a62217877cff0f97aaf5362907ae52c651dc4bc
22-ncore-020g <22-ncore-020g@22-ncore-020>
2023-03-21 17:47:19 +0900

    이미지 업로드 기능구현중

--- ctor/src/main/java/com/ctor/controller/BlindController.java
+++ ctor/src/main/java/com/ctor/controller/BlindController.java
@@ -1,12 +1,34 @@
 package com.ctor.controller;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FilenameUtils;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ctor.dto.BlindDTO;
@@ -14,9 +36,11 @@ import com.ctor.dto.BlindPageRequestDTO;
 import com.ctor.service.BlindService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @Controller
 @RequiredArgsConstructor
+@Log4j2
 public class BlindController {
 
 	@Autowired
@@ -46,4 +70,39 @@ public class BlindController {
 		redirectAttributes.addAttribute("bno",bno);
 		return "redirect:blindRead";
 	}
-}
+
+
+	    @PostMapping("/fileUpload")
+	    @ResponseBody
+	    public String handleImageUpload(@RequestParam("upload") MultipartFile file,HttpServletResponse res,HttpServletRequest req) {
+	    	UUID uuid = UUID.randomUUID();
+	    	PrintWriter printWriter = null;
+	    	OutputStream out = null;
+	    	byte[] bytes;
+			try {
+				bytes = file.getBytes();
+	    	String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+	    	 String imgUploadPath = "img"+ File.separator+"upload" + File.separator + uuid + "." + extension;
+	    	 out = new FileOutputStream(imgUploadPath);
+	            out.write(bytes);
+	            out.flush();
+	            
+	            printWriter = res.getWriter();
+	            String callback = req.getParameter("CKEditorFuncNum");
+	            String fileUrl = "img/upload/" + uuid + "." + extension;
+
+	            printWriter.println("<script type='text/javascript'>"
+	                    + "window.parent.CKEDITOR.tools.callFunction("
+	                    + callback+",'"+ fileUrl+"','이미지를 업로드하였습니다.')"
+	                    +"</script>");
+
+	            printWriter.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        return null;
+	    }
+
+	}
+	    
