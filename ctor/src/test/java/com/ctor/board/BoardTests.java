7a86e7e34a6b9d2f1ccc3f0afa1f7531c0347b07
18-cocp-122g <18-cocp-122g@18-cocp-122>
2023-03-22 11:02:15 +0900

    Test: 수정된 보드 조회쿼리 테스트

--- ctor/src/test/java/com/ctor/board/BoardTests.java
+++ ctor/src/test/java/com/ctor/board/BoardTests.java
@@ -15,6 +15,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
+import javax.persistence.Tuple;
+
 import org.apache.naming.java.javaURLContextFactory;
 import org.aspectj.weaver.NewConstructorTypeMunger;
 import org.junit.jupiter.api.Test;
@@ -41,11 +43,11 @@ public class BoardTests {
 //	@Commit
 //	public void BoardTests() {
 //
-//		IntStream.rangeClosed(1, 5).forEach(i->{
-////			Member member = Member.builder().email("aaa"+i+"@green.com").name("테스터"+i).build();
-////			kakaoRepository.save(member);
-//			
-//			String myString = "20230331";
+////		IntStream.rangeClosed(1, 5).forEach(i->{
+//////			Member member = Member.builder().email("aaa"+i+"@green.com").name("테스터"+i).build();
+//////			kakaoRepository.save(member);
+////			
+//			String myString = "20230404";
 //			SimpleDateFormat dtFormat = new SimpleDateFormat("yyyyMMdd");
 //			Date mydate = null;
 //			try {
@@ -56,20 +58,20 @@ public class BoardTests {
 //			}
 //			
 //			BoardDTO boardDTO = BoardDTO.builder()
-//					.title("스터디 모집 "+i)
+//					.title("스터디 모집 ")
 //					.text("프로젝트 상세내용")
-//					.category("스터디")
+//					.category("프로젝트")
 //					.closingDate(mydate.toString())
 //					.duration("3w")
 //					.groupMember((int)(Math.random()*5+2))
-//					.position("백엔드,프론트엔드,디자이너")
-//					.techStack("JAVA,JSP,MariaDB")
+//					.position("백엔드,디자이너")
+//					.techStack("JAVA,JSP")
 //					.hasTutor(false)
 //					.closed(false)
-//					.memEmail("moonjikwang@naver.com")
+//					.memEmail("aaa@green.com")
 //					.build();
 //			boardService.write(boardDTO);
-//		});
+////		});
 //		
 //		
 //	}
@@ -124,30 +126,55 @@ public class BoardTests {
 //	public void delTest() {
 //		boardService.delete(3l);
 //	}
-//	@Test
-//	@Transactional
-//	public void findTest() {
-//		List<BoardDTO> dtoList =
-//		boardService.findByEmail("aaa@naver.com");
-//		System.out.println(dtoList);
-//	}
+	@Test
+	@Transactional
+	public void findTest() {
+	//레포지토리 테스트
+//		List<Object[]> result = boardRepository.getBoardByTech("JAVA");
+//		result.forEach(arr->{
+//			System.out.println((Board)arr[0]);
+//			System.out.println((Member)arr[1]);
+//			System.out.println((Long)arr[2]);
+//			System.out.println((Long)arr[3]);
+//		});
+//		List<Object[]> result = boardRepository.getBoardByPosition("프론트엔드");
+//		result.forEach(arr->{
+//			System.out.println((Board)arr[0]);
+//			System.out.println((Member)arr[1]);
+//			System.out.println((Long)arr[2]);
+//			System.out.println((Long)arr[3]);
+//		});
+		
+	//서비스 테스트
+		List<BoardDTO> dtoList =
+		boardService.findByTech("MariaDB");
+		System.out.println(dtoList);
+		dtoList.forEach(b->System.out.println(b));
+	}
 //	@Test
 //	public void findTest() {
 //		List<BoardDTO> dtoList =
 //		boardService.findByPosition("디자이너");
 //		dtoList.forEach(dto->System.out.println(dto));
 //	}
-	@Test
-	public void findTest() {
+//	@Test
+//	@Transactional
+//	public void findTest() {
 //		BoardDTO dto =
 //		boardService.findByBno(5L);
 //		System.out.println(dto);
-		
-//		boardRepository.getBoardByBoardno(5L);
-		System.out.println(boardRepository.getBoardByBoardno(5L).length);
-	}
+//		
+////		Tuple result = boardRepository.getBoardByBoardno(5L);
+////		Board board = (Board) result.get(0);
+////		Member member = (Member) result.get(1);
+////		int c = ((Long)result.get(2)).intValue();
+////		int p = ((Long)result.get(3)).intValue();
+////		System.out.println(board);
+////		System.out.println(member);
+////		System.out.println(c);
+////		System.out.println(p);
+//	}
 //	@Test
-//	@Transactional
 //	public void findTest() {
 //		List<BoardDTO> dtoList =
 //		boardService.findAllBoards();
