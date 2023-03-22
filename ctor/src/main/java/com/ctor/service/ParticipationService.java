cdb9abf8cfa53de5f54e48cb863b2366f7c44f24
YuHyeon <yh999886@gmail.com>
2023-03-22 17:41:58 +0900

    Feat: 참여 신청, 취소, 프로젝트별 참여자 조회, 회원이 참여중인 내역 보기 기능 추가

---
+++ ctor/src/main/java/com/ctor/service/ParticipationService.java
@@ -0,0 +1,42 @@
+package com.ctor.service;
+
+import java.util.List;
+
+import com.ctor.dto.ParticipationDTO;
+import com.ctor.entity.Board;
+import com.ctor.entity.Member;
+import com.ctor.entity.Participation;
+
+public interface ParticipationService {
+
+	Long participate(ParticipationDTO dto);	//참여신청하기(insert)
+	Long cancel(Long pno);	//신청 취소하기(delete)
+	
+	List<ParticipationDTO> findByBno(Long bno);	//플젝,스터디로 조회하기
+	List<ParticipationDTO> findByEmail(String email);	//참여자로 조회하기
+	
+	default ParticipationDTO entityToDTO(Participation p) {
+		ParticipationDTO dto = ParticipationDTO.builder()
+				.pNo(p.getPNo())
+				.pRegDate(p.getRegDate())
+				.pModDate(p.getModDate())
+				.pBno(p.getBoard().getBoardno())
+				.pMemEmail(p.getMember().getEmail())
+				.pNickName(p.getMember().getNickName())
+				.pProfileImg(p.getMember().getProfileImg())
+				.build();
+		return dto;
+		
+	}
+	
+	default Participation dtoToEntity(ParticipationDTO dto) {
+		Participation p = Participation.builder()
+				.pNo(dto.getPNo())
+				.board(Board.builder().boardno(dto.getPBno()).build())
+				.member(Member.builder().email(dto.getPMemEmail()).build())
+				.build();
+		return p;
+	}
+	
+	
+}
