58b9e8461f2066aecd6b06dc568419c1b7e3d230
YuHyeon <yh999886@gmail.com>
2023-03-22 17:41:58 +0900

    Feat: 참여 신청, 취소, 프로젝트별 참여자 조회, 회원이 참여중인 내역 보기 기능 추가

--- ctor/src/main/java/com/ctor/repository/ParticipationRepository.java
+++ ctor/src/main/java/com/ctor/repository/ParticipationRepository.java
@@ -1,10 +1,32 @@
 package com.ctor.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ctor.entity.Participation;
 
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
 
+	//원글번호로 글에 딸린 참여자를 조회
+		@Query(value = "SELECT p "
+				+ "FROM Participation p "
+				+ "LEFT JOIN FETCH p.member "
+				+ "LEFT JOIN FETCH p.board "
+				+ "WHERE p.board.boardno =:bno "
+				+ "ORDER BY p.regDate DESC")
+		List<Participation> getByBno(@Param("bno")Long bno);
+		
+	//회원이 참여중인 내용을 조회
+		@Query(value = "SELECT p "
+				+ "FROM Participation p "
+				+ "LEFT JOIN FETCH p.member "
+				+ "LEFT JOIN FETCH p.board "
+				+ "WHERE p.member.email =:email "
+				+ "ORDER BY p.regDate DESC")
+		List<Participation> getByEmail(@Param("email")String email);
 	
+		
 }
