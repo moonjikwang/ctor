e31be5ab1a2b48206e8210db6508297a87f5983f
18-cocp-122g <18-cocp-122g@18-cocp-122>
2023-03-22 14:28:40 +0900

    Feat: 보드 댓글 서비스 추가

---
+++ ctor/src/main/java/com/ctor/service/BoardCommentsService.java
@@ -0,0 +1,43 @@
+package com.ctor.service;
+
+import java.util.List;
+
+import com.ctor.dto.BoardCommentsDTO;
+import com.ctor.entity.Board;
+import com.ctor.entity.BoardComments;
+import com.ctor.entity.Member;
+
+public interface BoardCommentsService {
+
+	Long write(BoardCommentsDTO dto);
+	Long modify(BoardCommentsDTO dto);
+	Long delete(Long boardno);
+	
+	List<BoardCommentsDTO> findByBno(Long boardno);	//원글번호로 글에 딸린 댓글을 조회
+	
+	default BoardComments dtoToEntity(BoardCommentsDTO commentsDTO) {
+		BoardComments comments = BoardComments.builder()
+				.bCommentNo(commentsDTO.getBCommentNo())
+				.bCommentText(commentsDTO.getBCommentText())
+				.board(Board.builder().boardno(commentsDTO.getBCommentBno()).build())
+				.member(Member.builder().email(commentsDTO.getBCommentWriterEmail()).build())
+				.build();
+		return comments;
+	}
+	
+	default BoardCommentsDTO entityToDTO(BoardComments boardComments) {
+		BoardCommentsDTO dto = BoardCommentsDTO.builder()
+				.bCommentNo(boardComments.getBCommentNo())
+				.bCommentBno(boardComments.getBoard().getBoardno())
+				.bCommentRegDate(boardComments.getRegDate())
+				.bCommentModDate(boardComments.getModDate())
+				.bCommentText(boardComments.getBCommentText())
+				.bCommentWriterEmail(boardComments.getMember().getEmail())
+				.bCommentNickName(boardComments.getMember().getNickName())
+				.bCommentProfileImg(boardComments.getMember().getProfileImg())
+				.build();
+		
+		return dto;
+		
+	}
+}
