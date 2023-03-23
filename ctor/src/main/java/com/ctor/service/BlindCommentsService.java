19106e8cdaa6e6f4748e5fdc4edefd1b03954c9e
22-ncore-020g <22-ncore-020g@22-ncore-020>
2023-03-23 17:03:54 +0900

    댓글삭제기능구현

--- ctor/src/main/java/com/ctor/service/BlindCommentsService.java
+++ ctor/src/main/java/com/ctor/service/BlindCommentsService.java
@@ -3,7 +3,6 @@ package com.ctor.service;
 import java.util.List;
 
 import com.ctor.dto.BlindCommentsDTO;
-import com.ctor.dto.BlindDTO;
 import com.ctor.entity.Blind;
 import com.ctor.entity.BlindComments;
 import com.ctor.entity.Member;
@@ -13,12 +12,10 @@ public interface BlindCommentsService {
 	Long register(BlindCommentsDTO blindCommentsDTO);//댓글 등록
 	List<BlindCommentsDTO> getList(Long bno); 		 //특정 게시물의 댓글 목록
 	void modify(BlindCommentsDTO blindCommentsDTO);  //댓글 수정
-<<<<<<< HEAD
+
 	void remove(Long cno); 							 //댓글삭제
 	BlindCommentsDTO getDTO(Long cno);
-=======
-	void remove(Long cno); 							 //댓글 삭제
->>>>>>> branch 'main' of https://github.com/moonjikwang/ctor.git
+
 	
 	//BlindCommentsDTO -> BlindComments객체로 변환
 	default BlindComments dtoToEntity(BlindCommentsDTO blindCommentsDTO) {
