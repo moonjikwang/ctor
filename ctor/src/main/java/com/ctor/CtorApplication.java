76a74cd79394c1c5bd9cce6faa2de4d11881c767
22-ncore-020g <22-ncore-020g@22-ncore-020>
2023-03-24 17:36:55 +0900

    불필요내용 제거

--- ctor/src/main/java/com/ctor/CtorApplication.java
+++ ctor/src/main/java/com/ctor/CtorApplication.java
@@ -1,8 +1,5 @@
 package com.ctor;
 
-import java.util.TimeZone;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,11 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class CtorApplication {
 
-    @PostConstruct
-    void started() {
-        // timezone UTC 셋팅
-        TimeZone.setDefault(TimeZone.getTimeZone("KST"));
-    }
 	public static void main(String[] args) {
 		SpringApplication.run(CtorApplication.class, args);
 	}
