8d7fcc64f106fce4ce2173bb75836ff7d893fa83
moonjikwang <shjk0315@gmail.com>
2023-03-24 16:41:34 +0900

    modal, 마이페이지 수정

--- ctor/src/main/java/com/ctor/CtorApplication.java
+++ ctor/src/main/java/com/ctor/CtorApplication.java
@@ -2,6 +2,8 @@ package com.ctor;
 
 import java.util.TimeZone;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +11,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class CtorApplication {
 
+    @PostConstruct
+    void started() {
+        // timezone UTC 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("KST"));
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(CtorApplication.class, args);
 	}
