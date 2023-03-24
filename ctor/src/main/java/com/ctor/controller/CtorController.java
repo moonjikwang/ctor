e5deece7878b0e1db62230b0473bb550f03613ed
moonjikwang <shjk0315@gmail.com>
2023-03-25 01:10:17 +0900

    스킬별 보기 완료,랜딩페이지 작업중

--- ctor/src/main/java/com/ctor/controller/CtorController.java
+++ ctor/src/main/java/com/ctor/controller/CtorController.java
@@ -1,7 +1,11 @@
 package com.ctor.controller;
 
+import static org.hamcrest.CoreMatchers.nullValue;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -10,11 +14,13 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.hibernate.hql.internal.ast.tree.SelectExpression;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ctor.dto.BoardDTO;
 import com.ctor.dto.MemberDTO;
@@ -37,79 +43,53 @@ public class CtorController {
 	public String home() {
 		return "redirect:/index";
 	}
-	@GetMapping("/index")
-	public void index(Model model, HttpServletRequest request) {
-		System.out.println("인덱스 get실행");
-		List<BoardDTO> dto = boardService.findAllBoards();
-		List<SkillDTO> skillList = skillService.getList();
+	
+	@GetMapping("about")
+	public void about() {
 		
+	}
+	
+	@GetMapping("/index")
+	public void index(Model model,@RequestParam(value = "checkedList", required = false) List<String> selectedSkills) {
+		
+		List<SkillDTO> skillList = skillService.getList();
+		List<BoardDTO> dto = new ArrayList<>();
 		Map<String, String> skillMap = new HashMap<>();
 		for(SkillDTO skill : skillList) {
 			skillMap.put(skill.getSkill(), skill.getColor());
 		}
 		
-		HttpSession session = request.getSession();
+		List<String> chkdSkill = selectedSkills;
 		
-		System.out.println("session.getAttribute('chkdSkill')");
-		System.out.println(session.getAttribute("chkdSkill"));
-		
-		//선택한 스킬이 없으면 모든 스킬 반환
-		if(session.getAttribute("chkdSkill") == null) {
-			System.out.println("선택된 스킬 없음");
-			List<String> allSkills = new ArrayList();
-			for (int i = 0; i < skillList.size(); i++) {
-				allSkills.add(skillList.get(i).getSkill()); 
-				System.out.println(allSkills.get(i));
+		if(chkdSkill!=null) {
+			List<BoardDTO> tempList = new ArrayList<>();
+			for (String skill : chkdSkill) {
+			    boardService.findByTech(skill).forEach(tempDTO ->{
+			    	tempList.add(tempDTO);
+			    });
 			}
-			session.setAttribute("chkdSkill", allSkills);
+			dto = tempList;
 		}else {
-			List<String> skillChk = new ArrayList<>();
-			for(int i = 0; i < ((List<String>)session.getAttribute("chkdSkill")).size(); i++) {
-				skillChk.add(((List<String>)session.getAttribute("chkdSkill")).get(i));
-			}
-			session.setAttribute("chkdSkill", skillChk);
-			System.out.println("skillChk");
-			System.out.println(skillChk);
+		dto = boardService.findAllBoards();
 		}
-		
-		
+		model.addAttribute("chkdSkill",chkdSkill);
 		model.addAttribute("dto",dto);
 		model.addAttribute("skillMap",skillMap);
 		model.addAttribute("skillList",skillList);
 		
 	}
+	@PostMapping("/index")
+	public String skillindex(@RequestParam(value = "skillChk") List<String> selectedSkills,RedirectAttributes redirectAttributes) {
+		redirectAttributes.addAttribute("checkedList",selectedSkills);
+		
+		return "redirect:/index";
+	}
+	
 	@GetMapping("/changelog")
 	public void changelog(Model model) {
 		List<Map<String, String>> committer = apiService.apicall();
 		model.addAttribute("list",committer);
 	}
 	
-	/**
-	 * 
-	 * 이하 내용을 스크립트에서 처리하도록 하고 삭제예정
-	 */
-	@PostMapping("/tab")
-	public String tab(@RequestParam(value = "skillChk", required = false) List<String> selectedSkills, HttpServletRequest request) {
-		System.out.println("post 실행");
-		List<SkillDTO> skillList = skillService.getList();
-		List<String> chkdSkill = selectedSkills;
-		//선택한 스킬이 없으면 모든 스킬 반환
-		if(selectedSkills == null || selectedSkills.size() == 0) {
-			List<String> allSkills = new ArrayList();
-			for (int i = 0; i < skillList.size(); i++) {
-				allSkills.add(skillList.get(i).getSkill()); 
-				System.out.println(allSkills.get(i));
-			}
-			chkdSkill = allSkills;
-		}
-		HttpSession session = request.getSession();
-		session.setAttribute("chkdSkill", chkdSkill);
-		
-		System.out.println("selectedSkills : "+selectedSkills);
-		System.out.println("chkdSkill : "+chkdSkill);
-		System.out.println(session.getAttribute("chkdSkill"));
-		return "redirect:/index";
-
-		
-	}
+	
 }
