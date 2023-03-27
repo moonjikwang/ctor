bb3ddd7b7e3834b0ac3f55d5a18bf5f9097e1e6e
YuHyeon <yh999886@gmail.com>
2023-03-27 14:34:14 +0900

    Feat: 직군탭 추가

--- ctor/src/main/java/com/ctor/controller/CtorController.java
+++ ctor/src/main/java/com/ctor/controller/CtorController.java
@@ -15,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ctor.dto.BoardDTO;
+import com.ctor.dto.JobGroupDTO;
 import com.ctor.dto.SkillDTO;
 import com.ctor.service.ApiService;
 import com.ctor.service.BoardService;
+import com.ctor.service.JobGroupService;
 import com.ctor.service.SkillService;
 
 import lombok.RequiredArgsConstructor;
@@ -29,6 +31,8 @@ public class CtorController {
 	private final BoardService boardService;
 	private final SkillService skillService;
 	private final ApiService apiService;
+	private final JobGroupService jobGroupService;
+	
 	@GetMapping("/")
 	public String home() {
 		return "redirect:index";
@@ -41,33 +45,23 @@ public class CtorController {
 	}
 	
 	@GetMapping("index")
-	public void index(Model model,@RequestParam(value = "checkedList", required = false) List<String> selectedSkills) {
+	public void index(Model model) {
 		
 		List<SkillDTO> skillList = skillService.getList();
-		List<BoardDTO> dto = new ArrayList<>();
+		List<BoardDTO> dto = boardService.findAllBoards();
+		List<JobGroupDTO> jobList = jobGroupService.getList();
+		
 		Map<String, String> skillMap = new HashMap<>();
 		for(SkillDTO skill : skillList) {
 			skillMap.put(skill.getSkill(), skill.getColor());
 		}
 		
-		List<String> chkdSkill = selectedSkills;
 		
-		if(chkdSkill!=null) {
-			Set<BoardDTO> tempSet = new HashSet<>();
-			for (String skill : chkdSkill) {
-			    boardService.findByTech(skill).forEach(tempDTO ->{
-			        tempSet.add(tempDTO);
-			    });
-			}
-			List<BoardDTO> tempList = new ArrayList<>(tempSet);
-			dto = tempList;
-		}else {
-		dto = boardService.findAllBoards();
-		}
-		model.addAttribute("chkdSkill",chkdSkill);
 		model.addAttribute("dto",dto);
 		model.addAttribute("skillMap",skillMap);
 		model.addAttribute("skillList",skillList);
+		model.addAttribute("positionList",jobList);
+		
 		
 	}
 	@PostMapping("index")
