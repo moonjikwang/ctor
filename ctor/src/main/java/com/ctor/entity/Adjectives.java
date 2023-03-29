8a4fbb5d1ae0cf0e5c802ed80d87bd6000144289
moonjikwang <shjk0315@gmail.com>
2023-03-29 10:22:58 +0900

    랜덤닉네임으로 변경

---
+++ ctor/src/main/java/com/ctor/entity/Adjectives.java
@@ -0,0 +1,23 @@
+package com.ctor.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Adjectives {
+	@Id
+	private Long id;		  	 
+	
+	private String adjective;   
+}
--- ctor/src/main/java/com/ctor/repository/KakaoRepository.java
+++ ctor/src/main/java/com/ctor/repository/KakaoRepository.java
@@ -1,5 +1,7 @@
 package com.ctor.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +13,5 @@ public interface KakaoRepository extends JpaRepository<Member, String>{
 	@Query("SELECT a FROM Member a WHERE a.email =:email")
 	Member getMemberWithEmail(@Param("email")String email);
 	
+	List<Member> findByNickName(String nickName);
 }
